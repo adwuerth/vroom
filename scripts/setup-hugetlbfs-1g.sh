2ecbc74b3949adf84214adbdc43c4888b8a3e948
Adrian <adrian.wuerth@outlook.de>
2024-07-16 09:53:13 +0200

    added 1g hugetlbfs setup script

---
+++ scripts/setup-hugetlbfs-1g.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p /mnt/huge1G
+(mount | grep /mnt/huge1G) > /dev/null || mount -t hugetlbfs hugetlbfs /mnt/huge1G
+for i in {0..7}
+do
+	if [[ -e "/sys/devices/system/node/node$i" ]]
+	then
+		echo 1 > /sys/devices/system/node/node$i/hugepages/hugepages-1048576kB/nr_hugepages
+	fi
+done
